87962b1b611d0e24f203a599c5db070af573cea8
GlacyalWolf <ismaelromero1999@gmail.com>
2019-05-11 22:38:36 +0200

    exercici 6

---
+++ src/Segment.java
@@ -0,0 +1,55 @@
+public class Segment {
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+    public Segment(){}
+    public Segment(int x1,int x2,int y1,int y2){
+        this.x1=x1;
+        this.x2=x2;
+        this.y1=y1;
+        this.y2=y2;
+    }
+
+    public int getX1() {
+        return x1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public void setX2(int x2) {
+        this.x2 = x2;
+    }
+
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
+
+    public void setY2(int y2) {
+        this.y2 = y2;
+    }
+
+    public double Longitud(Segment a){
+        double longitut;
+        longitut=Math.sqrt(Math.pow(this.x1-this.x2, 2) + Math.pow(this.y1-this.y2, 2));
+        return longitut;
+    }
+
+
+
+}
---
+++ src/TestSegment.java
@@ -0,0 +1,21 @@
+import org.junit.*;
+import static org.junit.Assert.*;    // importa una classe static
+public class TestSegment {
+    @Test
+    public void testSegmentConstructorSimple(){
+        Segment s=new Segment();
+    }
+    @Test
+    public void testSegmentConstructorComplex(){
+        Segment s=new Segment(1,2,3,5);
+    }
+    @Test
+    public void testSegmentLongitud(){
+        Segment s=new Segment(1,2,3,5);
+        s.Longitud(s);
+    }
+    public static void main(String args[]) {
+        org.junit.runner.JUnitCore.main("TestSegment");
+    }
+}
+
