9e456b02f354eed8e645acad3117223032296c05
GlacyalWolf <ismaelromero1999@gmail.com>
2019-05-12 18:52:48 +0200

    exercici 7

--- src/Punt.java
+++ src/Punt.java
@@ -1,29 +1,29 @@
 public class Punt {
-    private int x;
-    private int y;
+    private int[] coord= new int[2];
+
     public Punt(){
 
     }
 
     public Punt(int x, int y) {
-        this.x=x;
-        this.y=y;
+        this.coord[0]=x;
+        this.coord[1]=y;
     }
 
     public int getX(){
-        return x;
+        return coord[0];
     }
     public int getY(){
-        return y;
+        return coord[1];
     }
     public void setX(int x){
-        this.x=x;
+        this.coord[0]=x;
     }
     public void setY(int y){
-        this.y=y;
+        this.coord[1]=y;
     }
     public void suma(Punt p) {
-        x+=p.x;
-        y+=p.y;
+        coord[0]+=p.coord[0];
+        coord[1]+=p.coord[1];
     }
 }
--- src/Segment.java
+++ src/Segment.java
@@ -1,52 +1,53 @@
 public class Segment {
-    private int x1;
+    private int[] coord1= new int[2];
+    private int[] coord2= new int[2];
     private int x2;
     private int y1;
     private int y2;
 
     public Segment(){}
     public Segment(int x1,int x2,int y1,int y2){
-        this.x1=x1;
-        this.x2=x2;
-        this.y1=y1;
-        this.y2=y2;
+        this.coord1[0]=x1;
+        this.coord2[0]=x2;
+        this.coord1[1]=y1;
+        this.coord2[1]=y2;
     }
 
     public int getX1() {
-        return x1;
+        return coord1[0];
     }
 
     public int getX2() {
-        return x2;
+        return coord2[0];
     }
 
     public int getY1() {
-        return y1;
+        return coord1[1];
     }
 
     public int getY2() {
-        return y2;
+        return coord2[1];
     }
 
     public void setX1(int x1) {
-        this.x1 = x1;
+        this.coord1[0] = x1;
     }
 
     public void setX2(int x2) {
-        this.x2 = x2;
+        this.coord2[0] = x2;
     }
 
     public void setY1(int y1) {
-        this.y1 = y1;
+        this.coord1[1] = y1;
     }
 
     public void setY2(int y2) {
-        this.y2 = y2;
+        this.coord2[1] = y2;
     }
 
     public double Longitud(Segment a){
         double longitut;
-        longitut=Math.sqrt(Math.pow(this.x1-this.x2, 2) + Math.pow(this.y1-this.y2, 2));
+        longitut=Math.sqrt(Math.pow(this.coord1[0]-this.coord2[0], 2) + Math.pow(this.coord1[1]-this.coord2[1], 2));
         return longitut;
     }
 
